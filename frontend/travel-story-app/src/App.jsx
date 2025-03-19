c7cd69248d2cfe84a328f585807b357f0cd4ad4d
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-03-19 07:30:52 +0700

    init base

---
+++ frontend/travel-story-app/src/App.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/dashboard" exact element={<Home />} />
+					<Route path="/login" exact element={<Login />} />
+					<Route path="/signup" exact element={<SignUp />} />
+				</Routes>
+			</Router>
+		</div>
+	);
+};
+
+export default App;
