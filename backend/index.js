7d6adef8b89b1d7f736fce167c4ac9f3ecf390f2
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-04-01 22:16:27 +0700

    API create-account

--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,65 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// const config = require("./config.json");
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
-// const express = require("express");
-// const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const config = require("./config.json");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-// const User = require("./models/user.model");
-// const TravelStory = require("./models/travelStory.model");
-// const { authenticateToken } = require("./utilities");
-// const upload = require("./multer");
-// const fs = require("fs");
-// const path = require("path");
+const User = require("./models/user.model");
+const TravelStory = require("./models/travelStory.model");
+const { authenticateToken } = require("./utilities");
+const upload = require("./multer");
+const fs = require("fs");
+const path = require("path");
 
-// mongoose.connect(config.connectionString);
+mongoose.connect(config.connectionString);
 
-// const app = express();
-// app.use(express.json());
-// app.use(cors({ origin: "*" }));
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-// // Create Account
-// app.post("/create-account", async (req, res) => {
-// 	const { fullName, email, password } = req.body;
+// Create Account
+app.post("/create-account", async (req, res) => {
+	const { fullName, email, password } = req.body;
 
-// 	if (!fullName || !email || !password) {
-// 		return res
-// 			.status(400)
-// 			.json({ error: true, message: "All fields are required" });
-// 	}
+	if (!fullName || !email || !password) {
+		return res
+			.status(400)
+			.json({ error: true, message: "All fields are required" });
+	}
 
-// 	const isUser = await User.findOne({ email });
-// 	if (isUser) {
-// 		return res
-// 			.status(400)
-// 			.json({ error: true, message: "User already exists" });
-// 	}
+	const isUser = await User.findOne({ email });
+	if (isUser) {
+		return res
+			.status(400)
+			.json({ error: true, message: "User already exists" });
+	}
 
-// 	const hashedPassword = await bcrypt.hash(password, 10);
+	const hashedPassword = await bcrypt.hash(password, 10);
 
-// 	const user = new User({
-// 		fullName,
-// 		email,
-// 		password: hashedPassword,
-// 	});
+	const user = new User({
+		fullName,
+		email,
+		password: hashedPassword,
+	});
 
-// 	await user.save();
-// 	const accessToken = jwt.sign(
-// 		{ userId: user._id },
-// 		process.env.ACCESS_TOKEN_SECRET,
-// 		{
-// 			expiresIn: "72h",
-// 		}
-// 	);
-// 	return res.status(201).json({
-// 		error: false,
-// 		user: { fullName: user.fullName, email: user.email },
-// 		accessToken,
-// 		message: "Registration Successful",
-// 	});
-// });
+	await user.save();
+	const accessToken = jwt.sign(
+		{ userId: user._id },
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: "72h",
+		}
+	);
+	return res.status(201).json({
+		error: false,
+		user: { fullName: user.fullName, email: user.email },
+		accessToken,
+		message: "Registration Successful",
+	});
+});
 
 // // Login
 // app.post("/login", async (req, res) => {
@@ -352,5 +352,5 @@
 // 		res.status(500).json({ error: true, message: error.message });
 // 	}
 // });
-// app.listen(8000);
-// module.exports = app;
+app.listen(8000);
+module.exports = app;
