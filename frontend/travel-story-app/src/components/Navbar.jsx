cedfda1a940cf861883a094c2ba05fde4f79a372
biennc <bienguyen3428@gmail.com>
2025-04-07 22:04:09 +0700

    create Navbar profile card

--- frontend/travel-story-app/src/components/Navbar.jsx
+++ frontend/travel-story-app/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
 import LOGO from '../assets/images/logo.svg'
+import { useNavigate } from 'react-router-dom'
+import ProfileInfo from './Cards/ProfileInfo'
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
+    const isTonken = localStorage.getItem("tonken")
+    const navigate = useNavigate()
+
+    const onLogout =() =>{
+        localStorage.clear()
+        navigate("/login")
+    }
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
         <img src={LOGO} alt='travel story' className='h-9'/>
+        
+        {isTonken && <ProfileInfo userInfo ={userInfo} onLogout={onLogout}/>}
     </div>
   )
 }
