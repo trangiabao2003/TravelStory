310b8af66a6d4cf553b5044356cef53835f9d69a
biennc <bienguyen3428@gmail.com>
2025-04-07 20:39:37 +0700

    Get user infor API integration

--- frontend/travel-story-app/src/pages/Home/Home.jsx
+++ frontend/travel-story-app/src/pages/Home/Home.jsx
@@ -1,9 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import axiosInstance from "../../utils/axiosInstance";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return <>
-  <Navbar/>
-  </>;
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  // Get User Info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        // Set user info if data exists
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401)
+        // Clear storage if unauthorized
+        localStorage.clear();
+      navigate("/login"); //redirect to login
+    }
+  };
+
+  useEffect (() => {
+    getUserInfo()
+
+    return () => {}
+  }, [])
+  return (
+    <>
+      <Navbar userInfo={userInfo}/>
+    </>
+  );
 };
 
 export default Home;
