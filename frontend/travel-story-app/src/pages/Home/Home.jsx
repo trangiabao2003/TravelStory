40bad3f837ff8b1f4911d2f3746b3d41c4009887
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-04-29 07:45:50 +0700

    Commit local changes

--- frontend/travel-story-app/src/pages/Home/Home.jsx
+++ frontend/travel-story-app/src/pages/Home/Home.jsx
@@ -11,140 +11,138 @@ import "react-toastify/dist/ReactToastify.css";
 import AddEditTravelStory from "./AddEditTravelStory";
 
 const Home = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [userInfo, setUserInfo] = useState(null);
-  const [allStories, setAllStories] = useState([]);
+	const [userInfo, setUserInfo] = useState(null);
+	const [allStories, setAllStories] = useState([]);
 
-  const [openAddEditModal, setOpenAddEditModal] = useState({
-    isShown: false,
-    type: "add",
-    data: null,
-  });
+	const [openAddEditModal, setOpenAddEditModal] = useState({
+		isShown: false,
+		type: "add",
+		data: null,
+	});
 
-  // Get User Info
-  const getUserInfo = async () => {
-    try {
-      const response = await axiosInstance.get("/get-user");
-      if (response.data && response.data.user) {
-        // Set user info if data exists
-        setUserInfo(response.data.user);
-      }
-    } catch (error) {
-      if (error.response.status === 401)
-        // Clear storage if unauthorized
-        localStorage.clear();
-      navigate("/login"); //redirect to login
-    }
-  };
+	// Get User Info
+	const getUserInfo = async () => {
+		try {
+			const response = await axiosInstance.get("/get-user");
+			if (response.data && response.data.user) {
+				// Set user info if data exists
+				setUserInfo(response.data.user);
+			}
+		} catch (error) {
+			if (error.response.status === 401)
+				// Clear storage if unauthorized
+				localStorage.clear();
+			navigate("/login"); //redirect to login
+		}
+	};
 
-  // Get all travel stories
-  const getAllTravelStories = async () => {
-    try {
-      const response = await axiosInstance.get("/get-all-stories");
-      if (response.data && response.data.stories) {
-        setAllStories(response.data.stories);
-      }
-    } catch (error) {
-      console.log("An unexpected error occurred. Please try again");
-    }
-  };
+	// Get all travel stories
+	const getAllTravelStories = async () => {
+		try {
+			const response = await axiosInstance.get("/get-all-stories");
+			if (response.data && response.data.stories) {
+				setAllStories(response.data.stories);
+			}
+		} catch (error) {
+			console.log("An unexpected error occurred. Please try again");
+		}
+	};
 
-  // Handle edit story click
-  const handleEdit = (data) => {};
+	// Handle edit story click
+	const handleEdit = (data) => {};
 
-  // Handle travel story click
-  const handleViewStory = (data) => {};
+	// Handle travel story click
+	const handleViewStory = (data) => {};
 
-  // Handle update favorite
-  const updateIsFavourite = async (storyData) => {
-    const storyID = storyData._id;
+	// Handle update favorite
+	const updateIsFavourite = async (storyData) => {
+		const storyID = storyData._id;
 
-    try {
-      const response = await axiosInstance.put(
-        "/update-is-favourite/" + storyID,
-        {
-          isFavourite: !storyData.isFavourite,
-        }
-      );
+		try {
+			const response = await axiosInstance.put(
+				"/update-is-favourite/" + storyID,
+				{
+					isFavourite: !storyData.isFavourite,
+				}
+			);
 
-      if (response.data && response.data.story) {
-        toast.success("Story Updated Successfully");
-        getAllTravelStories();
-      }
-    } catch (error) {
-      console.log("An unexpected error occurred. Please try again");
-    }
-  };
+			if (response.data && response.data.story) {
+				toast.success("Story Updated Successfully");
+				getAllTravelStories();
+			}
+		} catch (error) {
+			console.log("An unexpected error occurred. Please try again");
+		}
+	};
 
-  useEffect(() => {
-    getAllTravelStories();
-    getUserInfo();
+	useEffect(() => {
+		getAllTravelStories();
+		getUserInfo();
 
-    return () => {};
-  }, []);
-  return (
-    <>
-      <Navbar userInfo={userInfo} />
-      <div className="container mx-auto py-10">
-        <div className="flex gap-7">
-          <div className="flex-1">
-            {allStories.length > 0 ? (
-              <div className="grid grid-cols-2 gap-4">
-                {allStories.map((item) => {
-                  return (
-                    <TravelStoryCard
-                      key={item._id}
-                      imgUrl={item.imageUrl}
-                      title={item.title}
-                      story={item.story}
-                      date={item.visitedDate}
-                      visitedLocation={item.visitedLocation}
-                      isFavourite={item.isFavourite}
-                      onEdit={() => handleEdit(item)}
-                      onClick={() => handleViewStory(item)}
-                      onFavouriteClick={() => updateIsFavourite(item)}
-                    />
-                  );
-                })}
-              </div>
-            ) : (
-              <>Empty Card here</>
-            )}
-          </div>
+		return () => {};
+	}, []);
+	return (
+		<>
+			<Navbar userInfo={userInfo} />
+			<div className="container mx-auto py-10">
+				<div className="flex gap-7">
+					<div className="flex-1">
+						{allStories.length > 0 ? (
+							<div className="grid grid-cols-2 gap-4">
+								{allStories.map((item) => {
+									return (
+										<TravelStoryCard
+											key={item._id}
+											imgUrl={item.imageUrl}
+											title={item.title}
+											story={item.story}
+											date={item.visitedDate}
+											visitedLocation={item.visitedLocation}
+											isFavourite={item.isFavourite}
+											onEdit={() => handleEdit(item)}
+											onClick={() => handleViewStory(item)}
+											onFavouriteClick={() => updateIsFavourite(item)}
+										/>
+									);
+								})}
+							</div>
+						) : (
+							<>Empty Card here</>
+						)}
+					</div>
 
-          <div className="w-[320px]"></div>
-        </div>
-      </div>
+					<div className="w-[320px]"></div>
+				</div>
+			</div>
 
-      {/* {Add and Edit travel story model} */}
-      <Modal
-        isOpen={openAddEditModal.isShown}
-        onRequestClose={() => {}}
-        style={{
-          overlay: {
-            backgroundColor: "rgba(0,0,0,0.2)",
-            zIndex: 999,
-          },
-        }}
-        appElement={document.getElementById("root")}
-        className="model-box"
-      >
-        {/* <AddEditTravelStory /> */}
-      </Modal>
+			{/* {Add and Edit travel story model} */}
+			<Modal
+				isOpen={openAddEditModal.isShown}
+				onRequestClose={() => {}}
+				style={{
+					overlay: {
+						backgroundColor: "rgba(0,0,0,0.2)",
+						zIndex: 999,
+					},
+				}}
+				appElement={document.getElementById("root")}
+				className="model-box">
+				{/* <AddEditTravelStory /> */}
+			</Modal>
 
-      <button
-        className="w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-cyan-400 fixed right-10 bottom-10"
-        onClick={() => {
-          setOpenAddEditModal({ isShown: true, type: "add", data: null });
-        }}
-      >
-        <MdAdd className="text-[32px] text-white" />
-      </button>
+			<button
+				className="w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-cyan-400 fixed right-10 bottom-10"
+				onClick={() => {
+					setOpenAddEditModal({ isShown: true, type: "add", data: null });
+				}}>
+				<MdAdd className="text-[32px] text-white" />
+			</button>
 
-      <ToastContainer />
-    </>
-  );
+			<ToastContainer />
+		</>
+	);
 };
 
 export default Home;
